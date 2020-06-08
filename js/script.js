7157023b12f215c7db2ae7a6a665718a308bec58
Renata <renata3947@mail.ru>
2020-06-08 22:14:27 +0300

    Modal window

--- js/script.js
+++ js/script.js
@@ -88,5 +88,22 @@ window.addEventListener('DOMContentLoaded', function() {
 
     setcLock('timer', deadline);
 
+    // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.add('more-splash');
+        document.body.style.overflow = '';
+    });
 
 });
